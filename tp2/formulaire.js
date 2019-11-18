57fb7644aa402e01619d919920c18dfd989ce316
Ibrahima <fofanaibrahimah@gmail.com>
2019-11-18 09:41:10 +0100

    dernière partie TP2

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -1,8 +1,34 @@
 
-var formValid = document.getElementById("bouton_valider");
-
-formValid.addEventListener('onclick', validation);
+/* variable pour gerer le click*/
 
 function validation(){
+
+    document.getElementById("error").innerHTML ="";
+    document.getElementById("error").style.display = "block";
+
+    document.getElementById("resultat").style.display = "none";
+
+    if (document.getElementById("nom").Value.length < 5) {
+        document.getElementById("error").innerHTML = "Le champ du nom est obligatoire";
+    }
+    else if (document.getElementById("prenom").Value.length < 5) {
+        document.getElementById("error").innerHTML = "Le champ prenom est obligatoire";
+    }
+
+    else if (document.getElementById("adresse").value.length < 5) {
+        document.getElementById("error").innerHTML = "Le champ adresse est obligatoire";
+    }
+
+    else if (document.getElementById("mail").value.length < 5) {
+        document.getElementById("error").innerHTML = "Le champ mail est obligatoire";
+    }
+
+    else {
+        document.getElementById("error").style.display = "none";
+        document.getElementById("resultat").style.display = "block";
+
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+    }
 
 }
